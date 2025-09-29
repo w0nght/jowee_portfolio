5df80bea9ec81446cf0f9c07edd7211f25721d3d
w0nght <wwongheitung@gmail.com>
2025-09-29 17:37:17 +1000

    Add Codeblock componment into portfolio case study

--- src/pages/CaseStudyPortfolio.js
+++ src/pages/CaseStudyPortfolio.js
@@ -1,4 +1,7 @@
 import Tag from "../components/Tag";
+import CodeBlock from "../components/CodeBlock";
+import CustomGallery from "../components/Gallery";
+import { portfolioImages } from "../data/caseStudyImages";
 
 export default function CaseStudyPortfolio() {
   return (
@@ -29,10 +32,43 @@ export default function CaseStudyPortfolio() {
           <span className="inline-code">ProjectCard</span> <b>component</b> that
           handles layout and styling for every project.
         </li>
+        <CodeBlock
+          language="js"
+          code={`<ProjectCard
+ title="Scrabble Helper"
+  description="A self-initiated web tool that helps users explore word combinations using filters, required letters, and wildcard support."
+  tags={tags}
+  images={images}
+  liveUrl="https://scrabble-helper.netlify.app/"
+  caseStudyUrl="/projects/scrabble-helper"
+/>`}
+        />
         <li>
           Used props to pass in each project’s title, description, tags,
           screenshots, and links.
         </li>
+        <CodeBlock
+          language="js"
+          code={`const tags = [
+    { label: "HTML", icon: "html" },
+    { label: "CSS", icon: "css" },
+    { label: "JavaScript (Vanilla & ES6+)", icon: "javascript" },
+    { label: "Git", icon: "git" },
+  ];`}
+        />
+        <CodeBlock
+          language="js"
+          code={`import { IMAGE_PATHS } from "../data/imagePaths";
+
+const { SCRABBLE, PORTFOLIO } = IMAGE_PATHS;
+
+const images = [
+  {
+    src: \`\${SCRABBLE}/dark_mode_view_1.png\`,
+    alt: "Scrabble Helper screenshot Dark Mode",
+  },
+];`}
+        />
         <li>
           Created four little “project functions” (like{" "}
           <span className="inline-code">ProjectScrabbleHelper</span>,{" "}
@@ -45,6 +81,20 @@ export default function CaseStudyPortfolio() {
           component that renders all four cards under the “Featured Projects”
           heading — making it clean, consistent, and easy to expand.
         </li>
+        <CodeBlock
+          language="js"
+          code={`export default function Projects() {
+  return (
+    <section id="projects">
+      <h2>Featured Projects</h2>
+      <ProjectScrabbleHelper />
+      <ProjectPortfolio />
+      <ProjectTAFE />
+      <ProjectALDI />
+    </section>
+  );
+}`}
+        />
       </ul>
       <h2>😄 Outcome</h2>
       <p>
@@ -61,6 +111,7 @@ export default function CaseStudyPortfolio() {
         all learn! 😊
       </p>
       <h2>Screenshots</h2>
+      <CustomGallery images={portfolioImages} />
       <ul>
         <li>(Coming soon)</li>
         {/* <li>Hero and navigation</li>
